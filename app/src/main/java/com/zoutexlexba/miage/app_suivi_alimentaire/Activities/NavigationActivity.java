77bafd01e58d24c1ef8ce63ba2ae7219187c1f22
Anasse Zougarh <zougarh.anasse@gmail.com>
2018-02-14 18:33:01 +0100

    ajout travail ouley

---
+++ app/src/main/java/com/zoutexlexba/miage/app_suivi_alimentaire/Activities/NavigationActivity.java
@@ -0,0 +1,84 @@
+package com.zoutexlexba.miage.app_suivi_alimentaire.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.zoutexlexba.miage.app_suivi_alimentaire.MainActivity;
+import com.zoutexlexba.miage.app_suivi_alimentaire.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        Button btdailyactivity = (Button) findViewById(R.id.btdailyactivity);
+        Button btgoal = (Button) findViewById(R.id.btgoal);
+        Button btaddmeal = (Button) findViewById(R.id.btaddmeal);
+        Button btpersonnalinfo = (Button) findViewById(R.id.btpersonalinfo);
+
+        btdailyactivity.setOnClickListener(btdailyactivityListener);
+        btgoal.setOnClickListener(btgoalListener);
+        btaddmeal.setOnClickListener(btaddmealListener);
+        btpersonnalinfo.setOnClickListener(btpersonnalinfoListener);
+    }
+
+    //Redirection vers la page Activité journalière
+    private OnClickListener btdailyactivityListener= new OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Log.d("debug","Daily activity");
+            Intent dailyactivityactivity = new Intent (NavigationActivity.this, DailyActivity.class);
+            startActivity(dailyactivityactivity);
+        }
+    };
+
+
+    //Redirection vers la page des objectifs
+    private OnClickListener btgoalListener= new OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Log.d("debug","Mes Objectifs");
+            Intent goalActivity = new Intent(NavigationActivity.this, GoalActivity.class);
+            startActivity(goalActivity);
+
+        }
+    };
+
+
+    //Redirection vers la page d'ajout de nouveau repas
+    private OnClickListener btaddmealListener= new OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Log.d("debug","Ajouter un repas");
+            Intent addmealActivity = new Intent(NavigationActivity.this, MainActivity.class);
+            startActivity(addmealActivity);
+        }
+    };
+
+
+
+    //Redirection vers la page d'édition des infos personnelles
+    private OnClickListener btpersonnalinfoListener;
+
+    {
+        btpersonnalinfoListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("debug", "Modifier Mes infos perso");
+                Intent personnalinfoActivity = new Intent(NavigationActivity.this, PersonnalInfoActivity.class);
+                startActivity(personnalinfoActivity);
+            }
+        };
+    }
+}
