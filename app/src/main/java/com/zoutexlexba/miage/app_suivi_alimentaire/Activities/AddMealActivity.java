d4b955ba374295c5d73378d4887d84d294bae968
Tessier <tessier.paulalexandre@orange.fr>
2018-03-26 11:47:53 +0200

    commit ajout list

--- app/src/main/java/com/zoutexlexba/miage/app_suivi_alimentaire/Activities/AddMealActivity.java
+++ app/src/main/java/com/zoutexlexba/miage/app_suivi_alimentaire/Activities/AddMealActivity.java
@@ -2,13 +2,19 @@ package com.zoutexlexba.miage.app_suivi_alimentaire.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.zoutexlexba.miage.app_suivi_alimentaire.Entity.Day;
 import com.zoutexlexba.miage.app_suivi_alimentaire.Entity.Food;
 import com.zoutexlexba.miage.app_suivi_alimentaire.Entity.FoodConsumed;
+import com.zoutexlexba.miage.app_suivi_alimentaire.Entity.Meal;
 import com.zoutexlexba.miage.app_suivi_alimentaire.MainActivity;
 import com.zoutexlexba.miage.app_suivi_alimentaire.R;
 import com.zoutexlexba.miage.app_suivi_alimentaire.Repository.DayRepository;
@@ -38,25 +44,61 @@ public class AddMealActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     private FoodConsumedRepository foodConsumedRepository;
     private FoodRepository foodRepository;
 
+    private Meal currentMeal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
 
+        final EditText input = (EditText) findViewById(R.id.input_nom);
+
         Intent intent = getIntent();
         Integer idMeal = intent.getIntExtra("idRepas",-1);
 
-        if(idMeal == -1){
-
+        final RuntimeExceptionDao<Meal, Integer> mealDao = getHelper().getRepasRuntimeDao();
+        currentMeal = mealDao.queryForId(idMeal);
+        if(currentMeal == null){
+            currentMeal = new Meal();
+            mealDao.create(currentMeal);
         }
 
-        ListView listView = (ListView) findViewById(R.id.foodListDaily);
+        input.setText(currentMeal.getNom());
+
+        input.addTextChangedListener(new TextWatcher() {
+            boolean _ignore = false;
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (_ignore)
+                    return;
+
+                _ignore = true; // prevent infinite loop
+                // Change your text here.
+                currentMeal.setNom(input.getText().toString());
+                mealDao.update(currentMeal);
+                _ignore = false; // release, so the TextWatcher start to listen again.
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+            }
+        });
+
+
+        ListView listView = (ListView) findViewById(R.id.foodListMeal);
         foodList =new ArrayList<HashMap<String,String>>();
 
         foodConsumedRepository = new FoodConsumedRepository();
         foodRepository = new FoodRepository();
 
-        List<FoodConsumed> listConso = foodConsumedRepository.findFoodConsumedByDate(dateStr,getHelper());
+        List<FoodConsumed> listConso = foodConsumedRepository.findFoodConsumedByIdMeal(currentMeal.getId(), getHelper());
         food = foodRepository.findFoodById(listConso, getHelper());
 
         HashMap<String,String> entete=new HashMap<String, String>();
@@ -79,7 +121,7 @@ public class AddMealActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     public void AjoutAliment(View view){
         Intent intent = new Intent(AddMealActivity.this, MainActivity.class);
 
-        intent.putExtra("idRepas", 0);
+        intent.putExtra("idRepas", currentMeal.getId());
 
         startActivity(intent);
     }
